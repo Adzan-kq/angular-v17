178b3f2e2f280894b19cc8437a9079e56923ac63
AdzanDev <madzannk648@gmail.com>
2023-11-17 09:45:32 +0700

    add styling navbar & add control flow component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { ControlFlowComponent } from './control-flow/control-flow.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+
+    // component
+    ControlFlowComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angularv17';
+  componentName = '';
+
+  componentAction(name: string) {
+    this.componentName = name;
+  }
 }
