4fa5e45e8222afd259766dd6072da5d46900a42a
AdzanDev <madzannk648@gmail.com>
2023-11-17 10:09:40 +0700

    add control flow - for statement

--- src/app/control-flow/control-flow.component.ts
+++ src/app/control-flow/control-flow.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.scss',
 })
 export class ControlFlowComponent {
   value = true;
+  listName: string[] = [];
+  queryname = '';
+
+  constructor() {
+    console.log('🚀 ~ query data: ', this.queryname);
+  }
+
+  saveName() {
+    if (this.queryname.trim() !== '') {
+      this.listName.push(this.queryname);
+      this.queryname = '';
+    }
+  }
 }
